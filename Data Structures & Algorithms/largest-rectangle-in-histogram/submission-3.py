a15065056e47e3b9bd003d57e971fad5aac0dce6
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2026-09-08 10:39:49 -0700

    Add: largest-rectangle-in-histogram - submission-3

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def NextSmallestElement(self, arr: List[int], n: int) -> List[int]:
+        res = [0] * n
+        stack = []
+
+        for i in range(n-1,-1,-1):
+            while stack and arr[i] <= arr[stack[-1]]:
+                stack.pop()
+            res[i] = stack[-1] if stack else n
+            stack.append(i)
+        return res
+
+    def PreviousSmallestElement(self, arr: List[int], n: int) -> List[int]:
+        res = [0] * n
+        stack = []
+
+        for i in range(n):
+            while stack and arr[i] <= arr[stack[-1]]:
+                stack.pop()
+            res[i] = stack[-1] if stack else -1
+            stack.append(i)
+        return res
+
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        nse = self.NextSmallestElement(heights,n)
+        pse = self.PreviousSmallestElement(heights, n)
+        max_area = 0
+
+        for i in range(n):
+            max_area = max(max_area, heights[i] * (nse[i] - pse[i]-1))
+        return max_area
+        
